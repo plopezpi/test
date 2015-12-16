ec0fd2afb8be971c272330216669e032c98f34eb
Blindhawk <blindhawk@gmail.com>
2015-12-16 13:49:36 +0100

    MyTest

---
+++ MyTest/android/src/com/pldp/mytest/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.pldp.mytest.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.pldp.mytest.MyTest;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyTest(), config);
+	}
+}
---
+++ MyTest/desktop/src/com/pldp/mytest/desktop/DesktopLauncher.java
@@ -0,0 +1,13 @@
+package com.pldp.mytest.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.pldp.mytest.MyTest;
+
+public class DesktopLauncher {
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyTest(), config);
+    }
+}
